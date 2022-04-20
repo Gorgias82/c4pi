4cd8340eaebca93d77841b24d245a8f607510f94
Gorgias82 <jvr1982@gmail.com>
2022-04-20 11:42:31 +0200

    6 files modified, 1 added

--- src/app/login/login-card/login-card.component.ts
+++ src/app/login/login-card/login-card.component.ts
@@ -1,93 +1,161 @@
-import { Component, Injectable,  OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription, throwIfEmpty } from 'rxjs';
 import { Hotel } from '../../shared/hotel.model';
 import { LoginService } from '../login.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 @Component({
   selector: 'app-login-card',
   templateUrl: './login-card.component.html',
-  styleUrls: ['./login-card.component.css']
+  styleUrls: ['./login-card.component.css'],
 })
-
 export class LoginCardComponent implements OnInit {
-
   color: string;
   colores: Array<string>;
+  palabrasTotales: Array<Array<String>>;
+  palabrasColor!: Array<String>;
+  colorFuente!: String;
+  visibilidad!: String;
   nombre!: string;
   private loginSub!: Subscription;
-  private passwordSub !: Subscription;
+  private passwordSub!: Subscription;
   errorNombre!: boolean;
   hoteles!: Hotel[];
-  password !: string;
-  errorPassword !: boolean;
-  constructor(public loginservice : LoginService, public router : Router) { 
-    this.color = "#FFFFFF";
-    this.colores = ["#FF0000","#00FF00","#0000FF","#FFFF00"];
-    
+  password!: string;
+  errorPassword!: boolean;
+  constructor(public loginservice: LoginService, public router: Router) {
+    this.color = '#FFFFFF';
+    this.colores = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00'];
+    this.palabrasTotales = [
+      [
+        'Energéticos',
+        'Autoritarios',
+        'Valientes',
+        'Ambiciosos',
+        'Pasionales',
+        'Realistas',
+      ],
+      [
+        'Creativos',
+        'Constantes',
+        'Pacientes',
+        'Empáticos',
+        'Espirituales',
+        'Democráticos',
+      ],
+      [
+        'Reflexivos',
+        'Analíticos',
+        'Tecnológicos',
+        'Realistas',
+        'Tranquilos',
+        'Solitarios',
+      ],
+      [
+        'Sociales',
+        'Comunicativos',
+        'Participativos',
+        'Imprevisibles',
+        'Intelectuales',
+        'Críticos',
+      ],
+    ];
   }
 
   ngOnInit() {
-       this.loginservice.getHoteles();
-    this.loginSub = this.loginservice.getHotelUpdateListener()
-    .subscribe((hoteles: Hotel[]) =>{
-      this.hoteles = hoteles;
-    })
-   this.cambiaColor();
-   setInterval(() => { this.cambiaColor(); },3000);
+    this.loginservice.getHoteles();
+    this.loginSub = this.loginservice
+      .getHotelUpdateListener()
+      .subscribe((hoteles: Hotel[]) => {
+        this.hoteles = hoteles;
+      });
+    this.cambiaColor();
+    setInterval(() => {
+      this.cambiaColor();
+    }, 3000);
   }
 
-
-  public cambiaColor() : void{
-    let n =  Math.floor((Math.random() * 4));
+  public cambiaColor(): void {
+    let n = Math.floor(Math.random() * this.colores.length);
     this.color = this.colores[n];
+    this.palabrasColor = this.palabrasTotales[n];
+    switch (n) {
+      case 0:
+        this.colorFuente = '#F7F9F9';
+        break;
+      case 1:
+        this.colorFuente = '#212F3D';
+        break;
+      case 2:
+        this.colorFuente = '#FBFCFC';
+        break;
+      case 3:
+        this.colorFuente = '#1C2833';
+        break;
+        default:
+          break;
+    }
+    this.visibilidad = 'hidden';
+    this.visibilidad = 'visible';
+    // if (n == 0 || n == 2) {
+    //   this.colorFuente = '#FFFFFF';
+    // } else {
+    //   this.colorFuente = this.color;
+    //   this.colorFuente = '#000000';
+    // }
   }
 
-  public getNombre() : string{
+  public getNombre(): string {
     return this.nombre;
   }
 
-   mandarNombre(){
-    this.loginservice.sendLogin({envio : this.nombre});
-    this.loginSub = this.loginservice.getLoginUpdatedListener()
-    .subscribe((message : boolean) =>{
-      alert("respuesta de bd nombre " + message);
-      
-      this.errorNombre = !message;
-    
-    })
+  mandarNombre() {
+    this.loginservice.sendLogin({ envio: this.nombre });
+    this.loginSub = this.loginservice
+      .getLoginUpdatedListener()
+      .subscribe((message: boolean) => {
+        alert('respuesta de bd nombre ' + message);
+
+        this.errorNombre = !message;
+      });
   }
-   mandarPassword(){
-    this.loginservice.sendPassword({nombre: this.nombre, password : this.password});
-    this.passwordSub = this.loginservice.getPasswordUpdatedListener()
-    .subscribe((message : boolean) =>{
-      alert("respuesta de bd password " + message);
-      this.errorPassword = !message;
-      
-    })
+  mandarPassword() {
+    this.loginservice.sendPassword({
+      nombre: this.nombre,
+      password: this.password,
+    });
+    this.passwordSub = this.loginservice
+      .getPasswordUpdatedListener()
+      .subscribe((message: boolean) => {
+        alert('respuesta de bd password ' + message);
+        this.errorPassword = !message;
+      });
   }
 
- async onSubmit(){
+  async onSubmit() {
     this.mandarNombre();
-    if(!this.errorNombre){   
-     this.mandarPassword();
+    if (!this.errorNombre) {
+      this.mandarPassword();
     }
-  
-    setTimeout(() => { this.redireccionar(); },1000);
-    
 
+    setTimeout(() => {
+      this.redireccionar();
+    }, 1000);
   }
- redireccionar(){
-  //  alert("redireccionar error nombre " + this.errorNombre + " error password " + this.errorPassword);
-     if(this.errorNombre === false && this.errorPassword === false){
-      alert("funcion nombre dentro comprobacion " + this.errorNombre + " password " + this.errorPassword);
+  redireccionar() {
+    //  alert("redireccionar error nombre " + this.errorNombre + " error password " + this.errorPassword);
+    if (this.errorNombre === false && this.errorPassword === false) {
+      alert(
+        'funcion nombre dentro comprobacion ' +
+          this.errorNombre +
+          ' password ' +
+          this.errorPassword
+      );
       this.loginservice.data = this.nombre;
       this.router.navigate(['/home/main']);
-    
-     }
+    }
   }
-
 }
