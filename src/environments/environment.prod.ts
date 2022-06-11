9a73905dae5c012a0e5ba51a82d9a867515832c4
jorge <jvr1982@gmail.com>
2022-06-11 11:04:57 +0200

    heroku2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: "http://c4pi-env-1.eba-qcmxjq5n.us-east-1.elasticbeanstalk.com/home/login"
+  baseUrl: 'https://c4pi.herokuapp.com/',
 };
